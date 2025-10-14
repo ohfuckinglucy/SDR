137f4558ef43877d7e3aee7b09b6aa226c283d38
ohfuckinglucy <errordings@gmail.com>
2025-10-14 17:14:53 +0700

    Чуть лучше

--- dev/build/audiotexp.py
+++ dev/build/audiotexp.py
@@ -1,19 +1,11 @@
 import numpy as np
-import librosa
-from pydub import AudioSegment
-import scipy.signal
+from scipy.io.wavfile import write
 
-with open("rx_samples.pcm", 'rb') as f:
-    pcm_data = f.read()
+# Read as int16 real samples (not complex!)
+samples = np.fromfile("rx_samples.pcm", dtype=np.int16)
 
+# Optional: decimate from 1e6 to 48e3
+from scipy.signal import decimate
+audio_48k = decimate(samples, 1_000_000 // 48_000).astype(np.int16)
 
-# pcm_data.tofile("ipanema.pcm")
-
-audio = AudioSegment(
-    data=pcm_data,
-    sample_width=2,      # 2 байта = 16 бит
-    frame_rate=44100,    # частота дискретизации
-    channels=1           # моно
-)
-
-audio.export("ipanemaEXP.mp3", format="mp3", bitrate="192k")
+write("output.wav", 48000, audio_48k)
