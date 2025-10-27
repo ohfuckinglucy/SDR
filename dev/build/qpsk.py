c24f3e0a66577e9bd0146dab4faf2e78a56ffa6b
Rxmantic <errordings@gmail.com>
2025-10-27 22:23:29 +0700

    Обновил QPSK

--- dev/build/qpsk.py
+++ dev/build/qpsk.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import lfilter
 
 def mapper(bits):
     if len(bits) % 2 != 0:
-        print("N Должно быть кратным 2")
-        exit()
+        bits = bits[:len(bits) -1]
         
     mapping = {
         (0, 0):  1 + 1j,
@@ -21,41 +21,69 @@ def mapper(bits):
 # Параметры сигнала
 
 N = 20
+L = 10
+fs = L * (N // 2) / (N // 2)
+f = fs/32
+t = np.linspace(0, 20, 10000)
 bits = np.random.randint(0, 2, size=N).tolist()
+
+b = np.ones(L)
+a = [1]
 
 symbols = mapper(bits)
 
-T = 1
-f = 1/T
-t = np.linspace(0, N/2 * T, 5000, endpoint=False)
-fs = 5000 / (N * T)
-phase = 0
+I = np.real(symbols)
+Q = np.imag(symbols)
 
-sig = np.zeros_like(t)
+I_upsampling = np.zeros(len(I)*L)
+Q_upsampling = np.zeros(len(Q)*L)
 
-for i in range(len(t)):
-    sym_i = int(t[i] // T)
-    symbol = symbols[sym_i]
-    
-    I = symbol.real
-    Q = symbol.imag
-    
-    sig[i] = I * np.cos(2 * np.pi * f * t[i]) - Q * np.sin(2 * np.pi * f * t[i])
+for i in range(0, len(I)):
+    I_upsampling[i*L] = I[i]
+    Q_upsampling[i*L] = Q[i]
 
-plt.subplot(2, 1, 1)
-plt.plot(t, sig)
-plt.title("Сигнал")
-plt.xlabel("Время, с")
-plt.ylabel("Амплитуда, А")
+I_filtered = lfilter(b, a, I_upsampling)
+Q_filtered = lfilter(b, a, Q_upsampling)
+
+t_base = np.arange(len(I_filtered)) / L
+
+
+s_rf = I_filtered * np.cos(2*np.pi*f*t_base) + Q_filtered * np.sin(2*np.pi*f*t_base)
+
+plt.plot(t_base, s_rf)
+plt.title('Модулированный радиосигнал QPSK')
+plt.xlabel('Время (с)')
+plt.ylabel('Амплитуда')
 plt.grid(True)
+plt.show()
 
-plt.subplot(2, 1, 2)
-plt.scatter([s.real for s in symbols], [s.imag for s in symbols])
-plt.title("созвездие QPSK")
-plt.xlabel("I")
-plt.ylabel("Q")
+# print(f'Исходные биты: {bits}')
+# print(f'Реальная часть: {I}')
+# print(f'Мнимая часть: {Q}')
+# print(f'Апсемплинг I: {I_upsampling}')
+# print(f'Апсемплинг Q: {Q_upsampling}')
+# print(f'Формфильтер I: {I_filtered}')
+# print(f'Формфильтер Q: {Q_filtered}')
+
+plt.plot(I_filtered)
+plt.title('Синфазная компонента после формирующего фильтра')
+plt.xlabel('Отсчёты')
+plt.ylabel('Амплитуда')
 plt.grid(True)
-plt.axhline(0, color='black', linewidth=1.5)
-plt.axvline(0, color='black', linewidth=1.5)
-plt.tight_layout()
+plt.show()
+
+plt.plot(Q_filtered)
+plt.title('Квадратурная компонента после формирующего фильтра')
+plt.xlabel('Отсчёты')
+plt.ylabel('Амплитуда')
+plt.grid(True)
+plt.show()
+
+indices = np.arange(L//2, len(I_filtered), L)
+plt.scatter(Q_filtered[indices], I_filtered[indices])
+plt.title('Созвездие QPSK')
+plt.xlabel('Q')
+plt.ylabel('I')
+plt.grid(True)
+plt.axis('equal')
 plt.show()
