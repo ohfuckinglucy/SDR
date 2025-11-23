55c33e23a5409fdb4fb9a0ab33f6d49afb4fe6a0
Rxmantic <errordings@gmail.com>
2025-11-23 15:25:22 +0700

    Feat. Добавил отчет по 1-ой практике

--- dev/build/graphs.py
+++ dev/build/graphs.py
@@ -5,16 +5,15 @@ print("Tx: 0\nRx: 1")
 choose = int(input())
 
 if (choose == 0):
-    data = np.fromfile("tx.pcm", dtype=np.int16)
+    data = np.fromfile("tx_5.pcm", dtype=np.int16)
 else:
-    data = np.fromfile("rx.pcm", dtype=np.int16)
+    data = np.fromfile("rx_5.pcm", dtype=np.int16)
 
 N = len(data) // 2
 
 samples = []
-
 for i in range(N):
-    samples.append((data[2*i]/np.max(data)) + 1j * (data[2*i+1]/np.max(data)))
+    samples.append((data[2*i]) + 1j * (data[2*i+1]))
 
 L = np.ones(10)
 sample = np.convolve(samples, L)
@@ -23,12 +22,11 @@ sample_10 = []
 for i in range(9, len(sample), 10):
     sample_10.append(sample[i])
 
-
-
 t = np.arange(len(sample_10))
 
 plt.figure()
-plt.plot(t, np.imag(sample_10), "r", label="Real")
+plt.plot(t, np.real(sample_10), "b", label="Real")
+plt.plot(t, np.imag(sample_10), "r", label="imag")
 plt.legend()
 plt.show()
 
@@ -39,21 +37,28 @@ plt.axvline(0)
 plt.show()
 
 bits = []
+threshold = 500
 
-for i in range(0, len(sample_10)):
-    if np.real(sample_10[i]) > 5:
-        if np.imag(sample_10[i]) > 5:
-            bits.append(0)
-            bits.append(0)
-        elif np.imag(sample_10[i]) < 5:
-            bits.append(1)
-            bits.append(1)
-    elif np.real(sample_10[i] < -5):
-        if np.imag(sample_10[i]) > 5:
-            bits.append(0)
-            bits.append(1)
-        elif np.imag(sample_10[i]) < -5:
-            bits.append(1)
-            bits.append(0)
+for s in sample_10:
+    if abs(s) < threshold:
+        continue
+    re = np.imag(s)
+    im = -np.real(s)
+    # re = np.real(s)
+    # im = np.imag(s)
+    if re >= 0 and im >= 0:
+        bits.append(0)
+        bits.append(0)
+    elif re < 0 and im >= 0:
+        bits.append(0)
+        bits.append(1)
+    elif re < 0 and im < 0:
+        bits.append(1)
+        bits.append(1)
+    else:
+        bits.append(1)
+        bits.append(0)
+
+print(len(bits))
 
 print(bits)
