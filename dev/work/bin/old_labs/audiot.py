272d3013dd866437e633a59cdf2823245ab0bcb0
Tenebre <errordings@gmail.com>
2026-02-24 18:33:50 +0700

    Fix. Delete unnecessary files

--- dev/work/bin/old_labs/audiot.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import librosa
-from pydub import AudioSegment
-
-y, sr = librosa.load("aaa.mp3", sr=44100, mono=True)
-
-pcm_data = (y * 32767).astype(np.int16)
-
-pcm_data.tofile("aaa.pcm")
